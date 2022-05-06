eea11bc5de97147a05c1d5668ab818284be58b22
Levani (NOSTRA) Khozrevanidze <levanikh888@gmail.com>
2022-05-07 02:29:02 +0400

    fixed some issues

--- js/main.js
+++ js/main.js
@@ -1,17 +1,58 @@
-var icon = document.getElementById("icon");
+// check for saved "WhiteTheme" in localStorage
+let WhiteTheme = localStorage.getItem("white-theme");
 
-icon.onclick = function () {
-    document.body.classList.toggle("white-theme");
-    if (document.body.classList.contains("white-theme")) {
-        icon.src = "img/moon.svg";
-        logo.src = "img/NOSTRA - black.svg";
-    } else {
-        icon.src = "img/sun.svg";
-        logo.src = "img/NOSTRA.svg";
-    }
+const WhiteThemeToggle = document.querySelector("#icon");
 
 
+const enableWhiteTheme = () => {
+  // 1. Add the class to the body
+  document.body.classList.add("white-theme");
+  if (document.body.classList.contains("white-theme")) {
+    icon.src = "img/moon.svg";
+    logo.src = "img/NOSTRA - black.svg";
+  } else {
+    icon.src = "img/sun.svg";
+    logo.src = "img/NOSTRA.svg";
+  }
+  // 2. Update WhiteTheme in localStorage
+  localStorage.setItem("white-theme", "enabled");
 }
+
+const disableWhiteTheme = () => {
+  // 1. Remove the class from the body
+  document.body.classList.remove("white-theme");
+  if (document.body.classList.contains("white-theme")) {
+    icon.src = "img/moon.svg";
+    logo.src = "img/NOSTRA - black.svg";
+  } else {
+    icon.src = "img/sun.svg";
+    logo.src = "img/NOSTRA.svg";
+  }
+  // 2. Update WhiteTheme in localStorage 
+  localStorage.setItem("white-theme", null);
+}
+
+// If the user already visited and enabled WhiteTheme
+// start things off with it on
+if (WhiteTheme === "enabled") {
+  enableWhiteTheme();
+}
+
+// When someone clicks the button
+WhiteThemeToggle.addEventListener("click", () => {
+  // get their WhiteTheme setting
+  WhiteTheme = localStorage.getItem("white-theme");
+
+  // if it not current enabled, enable it
+  if (WhiteTheme !== "enabled") {
+    enableWhiteTheme();
+    // if it has been enabled, turn it off  
+  } else {
+    disableWhiteTheme();
+  }
+});
+
+
 
 /* header burger */
 
@@ -19,8 +60,7 @@ let headerBurger = document.querySelector(".header-burger");
 let menu = document.querySelector(".menu");
 let body = document.querySelector("body");
 headerBurger.onclick = function () {
-    headerBurger.classList.toggle("active");
-    menu.classList.toggle("active");
-    body.classList.toggle("no-scroll");
+  headerBurger.classList.toggle("active");
+  menu.classList.toggle("active");
+  body.classList.toggle("no-scroll");
 }
-
